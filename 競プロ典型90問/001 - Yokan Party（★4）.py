d64c5823933a9788aa0c5004a83d87159592fc5b
Yuta Takahashi <takahashi-yuta@kadokawa.jp>
2023-02-01 01:07:58 +0900

    Update 001 - Yokan Party（★4）.py

--- 競プロ典型90問/001 - Yokan Party（★4）.py
+++ 競プロ典型90問/001 - Yokan Party（★4）.py
@@ -1,27 +1,45 @@
-# N, L = list(map(int, input().split()))
-# K = int(input())
-# AN = list(map(int, input().split()))
-
-N, L = 3, 34
-K = 1
-AN = [8, 13, 26]
+N, L = list(map(int, input().split()))
+K = int(input())
+AN = list(map(int, input().split()))
 
 dn = []
 for i, a in enumerate(AN):
-
     if i == 0:
         dn.append(a)
     else:
         dn.append(a - AN[i - 1])
 dn.append(L - AN[-1:][0])
+# print(dn)
 
 
 def canCut(m):
-    print(m)
-    print(dn)
-    cut_count=0
+    cut_count = K
+    sum_count = 0
     for i, d in enumerate(dn):
-        left = 
+        sum_count += d
+        if sum_count >= m:
+            if cut_count != 0:
+                cut_count -= 1
+                sum_count = 0
+
+    if sum_count >= m:
+        # print(cut_count, sum_count, True)
+        return True
+    else:
+        # print(cut_count, sum_count, False)
+        return False
 
 
-canCut(1)
+found = False
+left, right = 1, L - 1
+
+while not found:
+    middle = int((right - left) / 2)
+    # print(left, middle, right)
+    if canCut(left + middle):
+        left += middle
+    else:
+        right -= middle
+    if middle == 0:
+        found = True
+print(left)
